5e646a0d58f047dfdfeb739ccf73fef5e2ce5c45
Petter Hareim <petter.hareim@bouvet.no>
2017-05-07 10:47:19 +0200

    test redeploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req,res){
-	res.send("<div style='margin-top: 3em; color:#888888; min-width: 200px; max-width:800px; margin-left: auto; margin-right:auto'><h1>Godt å se deg, kompis!</h1></div>");
+	res.send("<div style='margin-top: 3em; color:#888888; min-width: 200px; max-width:800px; margin-left: auto; margin-right:auto'><h1>Godt å se deg, kompis!</h1><h2>Her vil det komme mer.</h2></div>");
 });
 
 app.listen(80, function(){
